868c7c1afea57676f8b8bff5611149ba6fd2a553
Ksenia Porfirieva <porfirieva.k@mail.ru>
2022-10-06 20:17:06 +0700

    game pairs

--- std/pairs/main.js
+++ std/pairs/main.js
@@ -1,8 +1,8 @@
 (function () {
-  createCards(16); // initial app
+  initialGame();
 
   function createCards(cardsCount) {
-    const numbersArray = createPairsArray(8);
+    const numbersArray = createPairsArray(cardsCount / 2);
     const randomNumbers = shuffleNumbers(numbersArray);
 
     for (let i = 0; i < cardsCount; i++) {
@@ -32,22 +32,78 @@
     return numbers;
   }
 
-  const cards = document.querySelectorAll(".card");
+  function restartGame() {
+    const restartBtn = document.createElement("button");
+    restartBtn.classList.add("btn");
+    restartBtn.textContent = "Сыграть еще раз";
 
-  cards.forEach((item) => {
-    item.addEventListener("click", (e) => {
-      e.target.classList.add("visible");
+    const clearField = document.querySelector(".container");
+    clearField.append(restartBtn);
 
-      let visibleCards = document.querySelectorAll(".visible");
-      // console.log(visibleCards.length);
-
-      if (visibleCards.length === 3) {
-        document.querySelectorAll(".visible").forEach((item) => {
-          item.classList.remove("visible");
-        });
+    restartBtn.addEventListener("click", () => {
+      while (clearField.firstChild) {
+        clearField.removeChild(clearField.firstChild);
       }
+      initialGame();
     });
-  });
+  }
 
-  // console.log(item.classList.contains("visible"));
+  function initialGame() {
+    const message = document.createElement("p");
+    message.classList.add("message");
+    message.textContent = "Сколько пар карточек создать?*";
+    const rules = document.createElement("p");
+    rules.textContent = "*Введите чётное число от 2 до 8";
+
+    const form = document.createElement("form");
+    const input = document.createElement("input");
+    input.classList.add("input");
+    const btnStart = document.createElement("input");
+    btnStart.setAttribute("type", "submit");
+    btnStart.setAttribute("value", "GO!");
+    btnStart.classList.add("btn");
+
+    form.append(input, btnStart);
+
+    document.querySelector(".container").append(message, form, rules);
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      let pairs = input.value;
+      const clearField = document.querySelector(".container");
+      while (clearField.firstChild) {
+        clearField.removeChild(clearField.firstChild);
+      }
+      createCards(pairs * 2);
+
+      const cards = document.querySelectorAll(".card");
+
+      let lastValue;
+      let lastCard;
+
+      cards.forEach((item) => {
+        item.addEventListener("click", () => {
+          item.classList.add("visible");
+          let visibleCards = document.querySelectorAll(".visible");
+          let currentValue = item.textContent;
+          item.classList.add("visible");
+
+          if (visibleCards.length % 2 !== 0) {
+            lastValue = currentValue;
+            lastCard = item;
+          } else if (visibleCards.length % 2 === 0) {
+            setTimeout(() => {
+              if (lastValue !== currentValue) {
+                item.classList.remove("visible");
+                lastCard.classList.remove("visible");
+              }
+              if (visibleCards.length === cards.length) {
+                restartGame();
+              }
+            }, 300);
+          }
+        });
+      });
+    });
+  }
 })();
